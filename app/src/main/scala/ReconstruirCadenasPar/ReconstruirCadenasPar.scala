3637114a3fc93c010138af3cbde16f888599babc
Stiven Henao <stivenhenaoa1@gmail.com>
2023-12-13 22:49:41 -0500

    Funcion PRC_IngenuoPar implementada.

--- app/src/main/scala/ReconstruirCadenasPar/ReconstruirCadenasPar.scala
+++ app/src/main/scala/ReconstruirCadenasPar/ReconstruirCadenasPar.scala
@@ -13,6 +13,26 @@ object ReconstruirCadenasPar {
   val alfabeto = Seq('a', 'c', 'g', 't')
   type Oraculo = Seq[Char] => Boolean
 
+  def PRC_IngenuoPar(umbral: Int)(n: Int, o: Oraculo): Seq[Char] = {
+    def generarSecuenciasRec(seq: Seq[Char], remaining: Int): LazyList[Seq[Char]] = {
+      if (remaining == 0) LazyList(seq)
+      else alfabeto.par.flatMap(char => generarSecuenciasRec(seq :+ char, remaining - 1)).to(LazyList)
+    }
+
+    val secuencias = generarSecuenciasRec(Seq.empty, n).par
+
+    def buscarEnSecuencia(secuencia: Seq[Char]): Boolean = {
+      o(secuencia)
+    }
+
+    if (n <= umbral) {
+      ReconstruirCadenas.PRC_Ingenuo(n, o)
+    } else {
+      val resultados = secuencias.filter(buscarEnSecuencia).seq
+      resultados.find(_.nonEmpty).getOrElse(Seq.empty[Char])
+    }
+  }
+
   
 
   
